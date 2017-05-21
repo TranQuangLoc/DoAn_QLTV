fc50b1c042d552ad5450879ea3510e10bd09d11a
trantrungan1306 <an.uno94@gmail.com>
2017-05-21 23:48:31 +0700

    tao form phieu tra sach

---
+++ DoAn-QuanLyThuVien/DoAn-QuanLyThuVien/PhieuTraSach.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace DoAn_QuanLyThuVien
+{
+    partial class fr_PhieuTraSach
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tt_phieuTraSach = new System.Windows.Forms.Label();
+            this.lb_tenDocGia = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_ngayTra = new System.Windows.Forms.Label();
+            this.lb_tienPhatKiNay = new System.Windows.Forms.Label();
+            this.lb_tienNo = new System.Windows.Forms.Label();
+            this.lb_tongNo = new System.Windows.Forms.Label();
+            this.txt_tenDocGia = new System.Windows.Forms.TextBox();
+            this.txt_tienPhatKiNay = new System.Windows.Forms.TextBox();
+            this.txt_tienNo = new System.Windows.Forms.TextBox();
+            this.txt_tongNo = new System.Windows.Forms.TextBox();
+            this.cbb_sachThieu = new System.Windows.Forms.ComboBox();
+            this.dtp_ngayTra = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_hoanTat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tt_phieuTraSach
+            // 
+            this.tt_phieuTraSach.AutoSize = true;
+            this.tt_phieuTraSach.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tt_phieuTraSach.ForeColor = System.Drawing.Color.Blue;
+            this.tt_phieuTraSach.Location = new System.Drawing.Point(205, 9);
+            this.tt_phieuTraSach.Name = "tt_phieuTraSach";
+            this.tt_phieuTraSach.Size = new System.Drawing.Size(210, 32);
+            this.tt_phieuTraSach.TabIndex = 0;
+            this.tt_phieuTraSach.Text = "Phiếu Trả Sách";
+            // 
+            // lb_tenDocGia
+            // 
+            this.lb_tenDocGia.AutoSize = true;
+            this.lb_tenDocGia.Location = new System.Drawing.Point(39, 68);
+            this.lb_tenDocGia.Name = "lb_tenDocGia";
+            this.lb_tenDocGia.Size = new System.Drawing.Size(88, 17);
+            this.lb_tenDocGia.TabIndex = 1;
+            this.lb_tenDocGia.Text = "Tên Độc Giả";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sách Thiếu";
+            // 
+            // lb_ngayTra
+            // 
+            this.lb_ngayTra.AutoSize = true;
+            this.lb_ngayTra.Location = new System.Drawing.Point(311, 67);
+            this.lb_ngayTra.Name = "lb_ngayTra";
+            this.lb_ngayTra.Size = new System.Drawing.Size(67, 17);
+            this.lb_ngayTra.TabIndex = 3;
+            this.lb_ngayTra.Text = "Ngày Trả";
+            // 
+            // lb_tienPhatKiNay
+            // 
+            this.lb_tienPhatKiNay.AutoSize = true;
+            this.lb_tienPhatKiNay.Location = new System.Drawing.Point(311, 117);
+            this.lb_tienPhatKiNay.Name = "lb_tienPhatKiNay";
+            this.lb_tienPhatKiNay.Size = new System.Drawing.Size(113, 17);
+            this.lb_tienPhatKiNay.TabIndex = 4;
+            this.lb_tienPhatKiNay.Text = "Tiền phạt kỳ này";
+            // 
+            // lb_tienNo
+            // 
+            this.lb_tienNo.AutoSize = true;
+            this.lb_tienNo.Location = new System.Drawing.Point(311, 161);
+            this.lb_tienNo.Name = "lb_tienNo";
+            this.lb_tienNo.Size = new System.Drawing.Size(58, 17);
+            this.lb_tienNo.TabIndex = 5;
+            this.lb_tienNo.Text = "Tiền Nợ";
+            // 
+            // lb_tongNo
+            // 
+            this.lb_tongNo.AutoSize = true;
+            this.lb_tongNo.Location = new System.Drawing.Point(314, 210);
+            this.lb_tongNo.Name = "lb_tongNo";
+            this.lb_tongNo.Size = new System.Drawing.Size(63, 17);
+            this.lb_tongNo.TabIndex = 6;
+            this.lb_tongNo.Text = "Tổng Nợ";
+            // 
+            // txt_tenDocGia
+            // 
+            this.txt_tenDocGia.Location = new System.Drawing.Point(132, 61);
+            this.txt_tenDocGia.Name = "txt_tenDocGia";
+            this.txt_tenDocGia.Size = new System.Drawing.Size(155, 22);
+            this.txt_tenDocGia.TabIndex = 7;
+            // 
+            // txt_tienPhatKiNay
+            // 
+            this.txt_tienPhatKiNay.Location = new System.Drawing.Point(436, 110);
+            this.txt_tienPhatKiNay.Name = "txt_tienPhatKiNay";
+            this.txt_tienPhatKiNay.Size = new System.Drawing.Size(200, 22);
+            this.txt_tienPhatKiNay.TabIndex = 10;
+            // 
+            // txt_tienNo
+            // 
+            this.txt_tienNo.Location = new System.Drawing.Point(436, 155);
+            this.txt_tienNo.Name = "txt_tienNo";
+            this.txt_tienNo.Size = new System.Drawing.Size(200, 22);
+            this.txt_tienNo.TabIndex = 11;
+            // 
+            // txt_tongNo
+            // 
+            this.txt_tongNo.Location = new System.Drawing.Point(436, 204);
+            this.txt_tongNo.Name = "txt_tongNo";
+            this.txt_tongNo.Size = new System.Drawing.Size(200, 22);
+            this.txt_tongNo.TabIndex = 12;
+            // 
+            // cbb_sachThieu
+            // 
+            this.cbb_sachThieu.FormattingEnabled = true;
+            this.cbb_sachThieu.Location = new System.Drawing.Point(132, 108);
+            this.cbb_sachThieu.Name = "cbb_sachThieu";
+            this.cbb_sachThieu.Size = new System.Drawing.Size(155, 24);
+            this.cbb_sachThieu.TabIndex = 13;
+            // 
+            // dtp_ngayTra
+            // 
+            this.dtp_ngayTra.Location = new System.Drawing.Point(436, 61);
+            this.dtp_ngayTra.Name = "dtp_ngayTra";
+            this.dtp_ngayTra.Size = new System.Drawing.Size(200, 22);
+            this.dtp_ngayTra.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 251);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(636, 150);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // btn_hoanTat
+            // 
+            this.btn_hoanTat.Location = new System.Drawing.Point(451, 423);
+            this.btn_hoanTat.Name = "btn_hoanTat";
+            this.btn_hoanTat.Size = new System.Drawing.Size(110, 35);
+            this.btn_hoanTat.TabIndex = 16;
+            this.btn_hoanTat.Text = "Hoàn Tất";
+            this.btn_hoanTat.UseVisualStyleBackColor = true;
+            // 
+            // fr_PhieuTraSach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 470);
+            this.Controls.Add(this.btn_hoanTat);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dtp_ngayTra);
+            this.Controls.Add(this.cbb_sachThieu);
+            this.Controls.Add(this.txt_tongNo);
+            this.Controls.Add(this.txt_tienNo);
+            this.Controls.Add(this.txt_tienPhatKiNay);
+            this.Controls.Add(this.txt_tenDocGia);
+            this.Controls.Add(this.lb_tongNo);
+            this.Controls.Add(this.lb_tienNo);
+            this.Controls.Add(this.lb_tienPhatKiNay);
+            this.Controls.Add(this.lb_ngayTra);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lb_tenDocGia);
+            this.Controls.Add(this.tt_phieuTraSach);
+            this.Name = "fr_PhieuTraSach";
+            this.Text = "Phiếu Trả Sách";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tt_phieuTraSach;
+        private System.Windows.Forms.Label lb_tenDocGia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_ngayTra;
+        private System.Windows.Forms.Label lb_tienPhatKiNay;
+        private System.Windows.Forms.Label lb_tienNo;
+        private System.Windows.Forms.Label lb_tongNo;
+        private System.Windows.Forms.TextBox txt_tenDocGia;
+        private System.Windows.Forms.TextBox txt_tienPhatKiNay;
+        private System.Windows.Forms.TextBox txt_tienNo;
+        private System.Windows.Forms.TextBox txt_tongNo;
+        private System.Windows.Forms.ComboBox cbb_sachThieu;
+        private System.Windows.Forms.DateTimePicker dtp_ngayTra;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_hoanTat;
+    }
+}
