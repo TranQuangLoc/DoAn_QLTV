1f9b6e341af5b86ff4a148ebfa870ccf1d8a413c
trantrungan1306 <an.uno94@gmail.com>
2017-05-21 15:52:04 +0700

    Tao form ThongTin Nhan Vien

---
+++ DoAn-QuanLyThuVien/DoAn-QuanLyThuVien/ThongTinNhanVien.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace DoAn_QuanLyThuVien
+{
+    partial class ThongTinNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_ThongTinNhanVien = new System.Windows.Forms.Label();
+            this.lb_HoTen = new System.Windows.Forms.Label();
+            this.lb_NgaySinh = new System.Windows.Forms.Label();
+            this.lb_bangCap = new System.Windows.Forms.Label();
+            this.lb_chucVu = new System.Windows.Forms.Label();
+            this.lb_dicChi = new System.Windows.Forms.Label();
+            this.lb_dienThoai = new System.Windows.Forms.Label();
+            this.lb_boPhan = new System.Windows.Forms.Label();
+            this.txt_hoTen = new System.Windows.Forms.TextBox();
+            this.txt_ngaySinh = new System.Windows.Forms.TextBox();
+            this.txt_bangCap = new System.Windows.Forms.TextBox();
+            this.txt_chucVu = new System.Windows.Forms.TextBox();
+            this.txt_diaChi = new System.Windows.Forms.TextBox();
+            this.txt_dienThoai = new System.Windows.Forms.TextBox();
+            this.txt_boPhan = new System.Windows.Forms.TextBox();
+            this.dtg_ThongTinNhanVien = new System.Windows.Forms.DataGridView();
+            this.btn_themNV = new System.Windows.Forms.Button();
+            this.btn_suaNV = new System.Windows.Forms.Button();
+            this.btn_xoaNV = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_ThongTinNhanVien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_ThongTinNhanVien
+            // 
+            this.lb_ThongTinNhanVien.AutoSize = true;
+            this.lb_ThongTinNhanVien.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_ThongTinNhanVien.ForeColor = System.Drawing.Color.Blue;
+            this.lb_ThongTinNhanVien.Location = new System.Drawing.Point(198, 9);
+            this.lb_ThongTinNhanVien.Name = "lb_ThongTinNhanVien";
+            this.lb_ThongTinNhanVien.Size = new System.Drawing.Size(283, 32);
+            this.lb_ThongTinNhanVien.TabIndex = 0;
+            this.lb_ThongTinNhanVien.Text = "Thông Tin Nhân Viên";
+            // 
+            // lb_HoTen
+            // 
+            this.lb_HoTen.AutoSize = true;
+            this.lb_HoTen.Location = new System.Drawing.Point(46, 77);
+            this.lb_HoTen.Name = "lb_HoTen";
+            this.lb_HoTen.Size = new System.Drawing.Size(69, 17);
+            this.lb_HoTen.TabIndex = 1;
+            this.lb_HoTen.Text = "Họ và tên";
+            // 
+            // lb_NgaySinh
+            // 
+            this.lb_NgaySinh.AutoSize = true;
+            this.lb_NgaySinh.Location = new System.Drawing.Point(46, 125);
+            this.lb_NgaySinh.Name = "lb_NgaySinh";
+            this.lb_NgaySinh.Size = new System.Drawing.Size(73, 17);
+            this.lb_NgaySinh.TabIndex = 2;
+            this.lb_NgaySinh.Text = "Ngày Sinh";
+            // 
+            // lb_bangCap
+            // 
+            this.lb_bangCap.AutoSize = true;
+            this.lb_bangCap.Location = new System.Drawing.Point(46, 180);
+            this.lb_bangCap.Name = "lb_bangCap";
+            this.lb_bangCap.Size = new System.Drawing.Size(70, 17);
+            this.lb_bangCap.TabIndex = 3;
+            this.lb_bangCap.Text = "Bằng Cấp";
+            // 
+            // lb_chucVu
+            // 
+            this.lb_chucVu.AutoSize = true;
+            this.lb_chucVu.Location = new System.Drawing.Point(46, 238);
+            this.lb_chucVu.Name = "lb_chucVu";
+            this.lb_chucVu.Size = new System.Drawing.Size(61, 17);
+            this.lb_chucVu.TabIndex = 4;
+            this.lb_chucVu.Text = "Chức Vụ";
+            // 
+            // lb_dicChi
+            // 
+            this.lb_dicChi.AutoSize = true;
+            this.lb_dicChi.Location = new System.Drawing.Point(300, 77);
+            this.lb_dicChi.Name = "lb_dicChi";
+            this.lb_dicChi.Size = new System.Drawing.Size(53, 17);
+            this.lb_dicChi.TabIndex = 5;
+            this.lb_dicChi.Text = "Địa Chỉ";
+            // 
+            // lb_dienThoai
+            // 
+            this.lb_dienThoai.AutoSize = true;
+            this.lb_dienThoai.Location = new System.Drawing.Point(300, 125);
+            this.lb_dienThoai.Name = "lb_dienThoai";
+            this.lb_dienThoai.Size = new System.Drawing.Size(77, 17);
+            this.lb_dienThoai.TabIndex = 6;
+            this.lb_dienThoai.Text = "Điện Thoại";
+            // 
+            // lb_boPhan
+            // 
+            this.lb_boPhan.AutoSize = true;
+            this.lb_boPhan.Location = new System.Drawing.Point(300, 183);
+            this.lb_boPhan.Name = "lb_boPhan";
+            this.lb_boPhan.Size = new System.Drawing.Size(62, 17);
+            this.lb_boPhan.TabIndex = 7;
+            this.lb_boPhan.Text = "Bộ Phận";
+            // 
+            // txt_hoTen
+            // 
+            this.txt_hoTen.Location = new System.Drawing.Point(126, 74);
+            this.txt_hoTen.Name = "txt_hoTen";
+            this.txt_hoTen.Size = new System.Drawing.Size(139, 22);
+            this.txt_hoTen.TabIndex = 8;
+            // 
+            // txt_ngaySinh
+            // 
+            this.txt_ngaySinh.Location = new System.Drawing.Point(126, 125);
+            this.txt_ngaySinh.Name = "txt_ngaySinh";
+            this.txt_ngaySinh.Size = new System.Drawing.Size(139, 22);
+            this.txt_ngaySinh.TabIndex = 9;
+            // 
+            // txt_bangCap
+            // 
+            this.txt_bangCap.Location = new System.Drawing.Point(126, 180);
+            this.txt_bangCap.Name = "txt_bangCap";
+            this.txt_bangCap.Size = new System.Drawing.Size(139, 22);
+            this.txt_bangCap.TabIndex = 10;
+            // 
+            // txt_chucVu
+            // 
+            this.txt_chucVu.Location = new System.Drawing.Point(126, 235);
+            this.txt_chucVu.Name = "txt_chucVu";
+            this.txt_chucVu.Size = new System.Drawing.Size(139, 22);
+            this.txt_chucVu.TabIndex = 11;
+            // 
+            // txt_diaChi
+            // 
+            this.txt_diaChi.Location = new System.Drawing.Point(417, 77);
+            this.txt_diaChi.Name = "txt_diaChi";
+            this.txt_diaChi.Size = new System.Drawing.Size(166, 22);
+            this.txt_diaChi.TabIndex = 12;
+            // 
+            // txt_dienThoai
+            // 
+            this.txt_dienThoai.Location = new System.Drawing.Point(417, 125);
+            this.txt_dienThoai.Name = "txt_dienThoai";
+            this.txt_dienThoai.Size = new System.Drawing.Size(166, 22);
+            this.txt_dienThoai.TabIndex = 13;
+            // 
+            // txt_boPhan
+            // 
+            this.txt_boPhan.Location = new System.Drawing.Point(417, 179);
+            this.txt_boPhan.Name = "txt_boPhan";
+            this.txt_boPhan.Size = new System.Drawing.Size(166, 22);
+            this.txt_boPhan.TabIndex = 14;
+            // 
+            // dtg_ThongTinNhanVien
+            // 
+            this.dtg_ThongTinNhanVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_ThongTinNhanVien.Location = new System.Drawing.Point(12, 291);
+            this.dtg_ThongTinNhanVien.Name = "dtg_ThongTinNhanVien";
+            this.dtg_ThongTinNhanVien.RowTemplate.Height = 24;
+            this.dtg_ThongTinNhanVien.Size = new System.Drawing.Size(620, 150);
+            this.dtg_ThongTinNhanVien.TabIndex = 15;
+            // 
+            // btn_themNV
+            // 
+            this.btn_themNV.Location = new System.Drawing.Point(63, 472);
+            this.btn_themNV.Name = "btn_themNV";
+            this.btn_themNV.Size = new System.Drawing.Size(75, 36);
+            this.btn_themNV.TabIndex = 16;
+            this.btn_themNV.Text = "Thêm";
+            this.btn_themNV.UseVisualStyleBackColor = true;
+            // 
+            // btn_suaNV
+            // 
+            this.btn_suaNV.Location = new System.Drawing.Point(278, 472);
+            this.btn_suaNV.Name = "btn_suaNV";
+            this.btn_suaNV.Size = new System.Drawing.Size(75, 36);
+            this.btn_suaNV.TabIndex = 17;
+            this.btn_suaNV.Text = "Sửa";
+            this.btn_suaNV.UseVisualStyleBackColor = true;
+            // 
+            // btn_xoaNV
+            // 
+            this.btn_xoaNV.Location = new System.Drawing.Point(508, 472);
+            this.btn_xoaNV.Name = "btn_xoaNV";
+            this.btn_xoaNV.Size = new System.Drawing.Size(75, 36);
+            this.btn_xoaNV.TabIndex = 18;
+            this.btn_xoaNV.Text = "Xóa";
+            this.btn_xoaNV.UseVisualStyleBackColor = true;
+            // 
+            // ThongTinNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 520);
+            this.Controls.Add(this.btn_xoaNV);
+            this.Controls.Add(this.btn_suaNV);
+            this.Controls.Add(this.btn_themNV);
+            this.Controls.Add(this.dtg_ThongTinNhanVien);
+            this.Controls.Add(this.txt_boPhan);
+            this.Controls.Add(this.txt_dienThoai);
+            this.Controls.Add(this.txt_diaChi);
+            this.Controls.Add(this.txt_chucVu);
+            this.Controls.Add(this.txt_bangCap);
+            this.Controls.Add(this.txt_ngaySinh);
+            this.Controls.Add(this.txt_hoTen);
+            this.Controls.Add(this.lb_boPhan);
+            this.Controls.Add(this.lb_dienThoai);
+            this.Controls.Add(this.lb_dicChi);
+            this.Controls.Add(this.lb_chucVu);
+            this.Controls.Add(this.lb_bangCap);
+            this.Controls.Add(this.lb_NgaySinh);
+            this.Controls.Add(this.lb_HoTen);
+            this.Controls.Add(this.lb_ThongTinNhanVien);
+            this.Name = "ThongTinNhanVien";
+            this.Text = "ThongTinNhanVien";
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_ThongTinNhanVien)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_ThongTinNhanVien;
+        private System.Windows.Forms.Label lb_HoTen;
+        private System.Windows.Forms.Label lb_NgaySinh;
+        private System.Windows.Forms.Label lb_bangCap;
+        private System.Windows.Forms.Label lb_chucVu;
+        private System.Windows.Forms.Label lb_dicChi;
+        private System.Windows.Forms.Label lb_dienThoai;
+        private System.Windows.Forms.Label lb_boPhan;
+        private System.Windows.Forms.TextBox txt_hoTen;
+        private System.Windows.Forms.TextBox txt_ngaySinh;
+        private System.Windows.Forms.TextBox txt_bangCap;
+        private System.Windows.Forms.TextBox txt_chucVu;
+        private System.Windows.Forms.TextBox txt_diaChi;
+        private System.Windows.Forms.TextBox txt_dienThoai;
+        private System.Windows.Forms.TextBox txt_boPhan;
+        private System.Windows.Forms.DataGridView dtg_ThongTinNhanVien;
+        private System.Windows.Forms.Button btn_themNV;
+        private System.Windows.Forms.Button btn_suaNV;
+        private System.Windows.Forms.Button btn_xoaNV;
+    }
+}
